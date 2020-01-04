42c988c02afd4a0958876410eb00c3e575111fe6
vamsi <vamsikrishna3415@outlook.in>
2020-01-04 22:27:26 +0530

    api versioning

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,42 @@
+class ItemsController < ApplicationController
+before_action :set_note
+before_action :set_note_item,only: [:show,:update,:destroy]
+
+
+def index
+  @items=@note.items
+  json_response(@items)
+end
+
+def create
+  @note.items.create!(item_params)
+  json_response(@note, :created)
+end
+
+def show
+  json_response(@item)
+end
+
+def update
+  @item.update(item_params)
+  head :no_content
+end
+
+def destroy
+  @item.destroy
+  head :no_content
+end
+
+private
+def item_params
+  params.permit(:name,:done,:deadline)
+end
+
+def set_note
+  @note=Note.find(params[:note_id])
+end
+
+def set_note_item
+  @item=@note.items.find_by!(id: params[:id]) if @note
+end
+end
---
+++ app/controllers/api/v1/notes_controller.rb
@@ -0,0 +1,44 @@
+class NotesController < ApplicationController
+
+  before_action  :set_note, only: [:show,:update,:destroy]
+
+def index
+  @notes=current_user.notes
+  json_response(@notes)
+end
+
+def create
+  @note=current_user.notes.create!(note_params)
+  json_response(@note,:created)
+end
+
+def show
+  json_response(@note)
+end
+
+def update
+  @note.update(note_params)
+  head :no_content
+end
+
+
+def destroy
+  @note.destroy
+  head :no_content
+end
+
+
+
+private
+
+def note_params
+  params.permit(:title)
+end
+
+def set_note
+  @note=Note.find(params[:id])
+end
+
+
+
+end
