ec3b1c0a079445bde6336c939840bfac1b2f2341
vamsi <vamsikrishna3415@outlook.in>
2020-01-04 22:29:00 +0530

    routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :notes do
+    resources :items
+  end
+
+  post 'auth/login', to: 'authentication#authenticate'
+  post 'signup', to: 'users#create'
 end
