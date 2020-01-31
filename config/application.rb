01d04050318a074320382ebfda0446c9153bfd31
vamsi <vamsikrishna3415@outlook.in>
2020-01-31 11:31:59 +0530

    allowed cross-origin updates and deletions

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module NoteMaker
     config.middleware.insert_before 0, Rack::Cors do
       allow do
          origins '*'
-         resource '*', :headers => :any, :methods => [:get, :post, :options]
+         resource '*', :headers => :any, :methods => [:get, :post,:put,:delete, :options]
        end
     end
     # Settings in config/environments/* take precedence over those specified here.
